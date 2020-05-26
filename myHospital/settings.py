d4f01cafb4b67430e006c052809461a395324f3f
Jatin Madan <39112299+stark3998@users.noreply.github.com>
2020-05-26 18:06:31 +0530

    Update Settings

--- myHospital/settings.py
+++ myHospital/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myHospital',
 		'USER': 'root',
-		'PASSWORD': 'Stark12345*',
+		'PASSWORD': '**********',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
